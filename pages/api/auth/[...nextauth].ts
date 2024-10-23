465ea79fdf386a133e82df9fa57c55a3bea0ea66
Tsotne Pharsenadze <wallemechson@gmail.com>
2024-10-24 00:51:22 +0400

    fixed some issues

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -57,7 +57,7 @@ export const authOptions: AuthOptions = {
   session: {
     strategy: "jwt",
   },
-  secret: process.env.AUTH_SECRET,
+  secret: process.env.NEXTAUTH_SECRET,
 };
 
 export default NextAuth(authOptions);
